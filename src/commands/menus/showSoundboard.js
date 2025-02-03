dd3abc569327dd3d6605de8ebfda31b2faf019de
Tim Wong <wongkk25@users.noreply.github.com>
2025-02-03 06:51:12 -0500

    converted menu to buttons

--- src/commands/menus/showSoundboard.js
+++ src/commands/menus/showSoundboard.js
@@ -1,48 +1,45 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('sb')
     .setDescription('Show the soundboard.'),
-  async execute(message) {
-    const listSounds = () => {
-      const folder = `./assets/sounds/sample`; // todo remove sample
-      return fs.readdirSync(folder).map(file => {
-        const name = file.split("_")[1]; // todo update indices
-        const emoji = file.split("_")[3].split('.')[0];
-        return { name, emoji };
-      });
+  async execute(interaction) {
+
+    // temporary
+    const getSoundName = (filename) => {
+      return filename.split("_")[1];
     };
 
-    const buildSoundboardMenu = (sounds) => {
-      const buildFields = (sounds) => {
-        const soundFields = sounds.map(sound => {
-          const soundName = sound.name.split('.')[0];
-          console.log(sound.emoji + ", " + soundName);
-          return { name: soundName, value: sound.emoji, inline: true }
-        });
-        const spaceField = { name: '\u200B', value: '\u200B' }
-        return [spaceField].concat(soundFields);
-      };
+    const buildButton = (soundName) => {
+      return new ButtonBuilder()
+        .setCustomId(soundName)
+        .setLabel(soundName)
+        .setStyle(ButtonStyle.Secondary);
+    };
 
-      const soundboardMenu = new EmbedBuilder()
-        .setColor('#4C18EB')
-        .setTitle('Soundboard')
-        .setDescription('Click on the corresponding button to play a sound.');
-      const fields = buildFields(sounds);
-      soundboardMenu.addFields(fields);
-      return soundboardMenu;
-    }
+    
+    const chunkButton = (acc, item, idx) => {
+      const chunkIdx = Math.floor(idx / numButtonsMax)
+      if(!acc[chunkIdx]) {
+        acc[chunkIdx] = []
+      }
+      acc[chunkIdx].push(item)
+      return acc
+    };
 
-    const sounds = listSounds();
-    const soundboardMenu = buildSoundboardMenu(sounds);
+    const numButtonsMax = 5; // discord limitation per row; 5 rows max for 25 sounds total
+    const folder = `./assets/sounds/sample`; // todo remove sample
+    const rows = fs.readdirSync(folder)
+      .map(getSoundName)
+      .map(buildButton)
+      .reduce(chunkButton, [])
+      .map(arr => new ActionRowBuilder().addComponents(arr));
 
-    message.channel.send({ embeds: [soundboardMenu] })
-      .then((sentMessage) => {
-        Object.values(sounds).forEach(val => {
-          sentMessage.react(val.emoji)
-        });
-      });
-  }
+    await interaction.reply({
+      content: `Click on the corresponding button to play a sound.`,
+      components: rows
+    });
+  },
 };
