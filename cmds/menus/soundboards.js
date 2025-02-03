fac40e1038a3159f400de77da83983303242f0da
Tim Wong <wongkk25@users.noreply.github.com>
2025-02-03 04:17:53 -0500

    initial cleanup of soundboard command

--- cmds/menus/soundboards.js
+++ cmds/menus/soundboards.js
@@ -2,7 +2,6 @@ const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
 const path = require('path');
 const Config = require('../../src/config')
-// const GIFS = Config.GIFS;
 
 // TODO for the love of god clean all this shit up
 
@@ -11,79 +10,51 @@ module.exports = {
     .setName('sb')
     .setDescription('Show the soundboard.'),
   async execute(message) {
-    // console.log(message)
-    //Instantiates a sounds object to store information about this characters sounds
-    let Sounds = new Object();
-
-    //func. that looks at the character folder and adds an entry to the Sounds obj.
-    function RetreiveAudioClips() {
+    const listSounds = () => {
+      const sounds = {};
       let i = 0;
-      // const folder = `./assets/sounds/${args}`;
-      const folder = `./assets/sounds/sample`;
+      const folder = `./assets/sounds/sample`; // todo remove sample
       fs.readdirSync(folder).forEach(file => {
         let character = file.split("_")[0]
         let name = file.split("_")[1]
         let emoji = file.split("_")[3].split('.')[0]
-        Sounds[i] = { character, name, emoji }
+        sounds[i] = { character, name, emoji }
         i++;
       })
+      return sounds;
     }
 
-    // Creates new character specific menu
-    const Menu = new EmbedBuilder()
-      .setColor('#4C18EB')
-      // .setTitle(`${args.charAt(0).toUpperCase() + args.slice(1)} Soundboard`)
-      .setTitle('Soundboard')
-      .setDescription('Click on the corresponding reaction to play a sound')
-      // .setThumbnail(`${GIFS[args]}`)
-      // .addFields({ name: '\u200B', value: '\u200B' })
+    const buildSoundboardMenu = (sounds) => {
+      const buildFields = (sounds) => {
+        const fields = [{ name: '\u200B', value: '\u200B' }];
+        let i = 0;
+        for (i; i < Object.keys(sounds).length; i++) {
+          let emoji = sounds[i].emoji;
+          let name = sounds[i].name.split('.')[0];
+          console.log(emoji + ", " + name);
+          fields.push({ name: name, value: emoji, inline: true });
+        };
+        return fields;
+      };
 
-    console.log(Sounds);
-
-    // Iterates through the Sounds obj. and append a field to the menu for every sound\
-    function BuildMenu() {
-      const fields = [
-        { name: '\u200B', value: '\u200B' }
-      ]
-      let i = 0;
-      for (i; i < Object.keys(Sounds).length; i++) {
-        // let character = Sounds[i].character
-        let emoji = Sounds[i].emoji;
-        let name = Sounds[i].name.split('.')[0];
-        console.log(emoji + ", " + name);
-        fields.push({ name: name, value: emoji, inline: true });
-      }
-      Menu.addFields(fields);
+      const soundboardMenu = new EmbedBuilder()
+        .setColor('#4C18EB')
+        .setTitle('Soundboard')
+        .setDescription('Click on the corresponding button to play a sound.');
+      const fields = buildFields(sounds);
+      soundboardMenu.addFields(fields);
+      return soundboardMenu;
     }
 
-    RetreiveAudioClips();
-    // console.log(Sounds);
-    BuildMenu();
-  
+    const sounds = listSounds();
+    console.log(sounds);
+    const soundboardMenu = buildSoundboardMenu(sounds);
 
-    // function NumberOfSounds() {
-    //   let SoundCount = Object.keys(Sounds).length;
-    //   let remainder = SoundCount % 3;
-
-    //   if (remainder === 0) {
-    //     return
-    //   } else if (remainder === 1) {
-    //     Menu.addField('-', "-", true)
-    //     Menu.addField('-', "-", true)
-    //   } else if (remainder === 2) {
-    //     Menu.addField('-', "-", true)
-    //   }
-    // }
-
-    // NumberOfSounds()
-
-    // console.log(Menu);
-
-    message.channel.send({ embeds: [Menu] })
+    message.channel.send({ embeds: [soundboardMenu] })
       .then((sentMessage) => {
-        Object.values(Sounds).forEach(val => {
+        Object.values(sounds).forEach(val => {
           sentMessage.react(val.emoji)
-        })
-      })
+        });
+      });
   }
 };
