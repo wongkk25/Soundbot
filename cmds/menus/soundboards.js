4da7cba0770f96e232f9317f2628fe1568e15b50
Tim Wong <wongkk25@users.noreply.github.com>
2025-02-03 02:17:19 -0500

    upgrade and rewrite for discordjs 14.7.3, swapped to json config, removed some unnecessary stuff for now

--- cmds/menus/soundboards.js
+++ cmds/menus/soundboards.js
@@ -1,22 +1,25 @@
-const { Discord, SlashCommandBuilder } = require('discord.js');
-const fs = require('fs');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
 const path = require('path');
 const Config = require('../../src/config')
-const GIFS = Config.GIFS;
+// const GIFS = Config.GIFS;
 
+// TODO for the love of god clean all this shit up
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('sb')
     .setDescription('Show the soundboard.'),
   async execute(message) {
+    // console.log(message)
     //Instantiates a sounds object to store information about this characters sounds
     let Sounds = new Object();
 
     //func. that looks at the character folder and adds an entry to the Sounds obj.
     function RetreiveAudioClips() {
       let i = 0;
-      const folder = `./assets/sounds/${args}`;
+      // const folder = `./assets/sounds/${args}`;
+      const folder = `./assets/sounds/sample`;
       fs.readdirSync(folder).forEach(file => {
         let character = file.split("_")[0]
         let name = file.split("_")[1]
@@ -27,44 +30,53 @@ module.exports = {
     }
 
     // Creates new character specific menu
-    const Menu = new Discord.MessageEmbed()
+    const Menu = new EmbedBuilder()
       .setColor('#4C18EB')
-      .setTitle(`${args.charAt(0).toUpperCase() + args.slice(1)} Soundboard`)
+      // .setTitle(`${args.charAt(0).toUpperCase() + args.slice(1)} Soundboard`)
+      .setTitle('Soundboard')
       .setDescription('Click on the corresponding reaction to play a sound')
-      .setThumbnail(`${GIFS[args]}`)
-      .addFields({ name: '\u200B', value: '\u200B' })
+      // .setThumbnail(`${GIFS[args]}`)
+      // .addFields({ name: '\u200B', value: '\u200B' })
 
     // Iterates through the Sounds obj. and append a field to the menu for every sound\
     function BuildMenu() {
+      const fields = [
+        { name: '\u200B', value: '\u200B' }
+      ]
       let i = 0;
       for (i; i < Object.keys(Sounds).length; i++) {
-        let character = Sounds[i].character
-        let emoji = Sounds[i].emoji
-        let name = Sounds[i].name.split('.')[0]
-        Menu.addField(`\`\`\`${emoji} ${name}\`\`\``, true)
+        // let character = Sounds[i].character
+        let emoji = Sounds[i].emoji;
+        let name = Sounds[i].name.split('.')[0];
+        fields.push({ name: name, value: emoji, inline: true });
       }
+      Menu.addFields(fields);
     }
 
-    RetreiveAudioClips()
-    BuildMenu()
+    RetreiveAudioClips();
+    // console.log(Sounds);
+    BuildMenu();
+  
 
-    function NumberOfSounds() {
-      let SoundCount = Object.keys(Sounds).length;
-      let remainder = SoundCount % 3;
+    // function NumberOfSounds() {
+    //   let SoundCount = Object.keys(Sounds).length;
+    //   let remainder = SoundCount % 3;
 
-      if (remainder === 0) {
-        return
-      } else if (remainder === 1) {
-        Menu.addField('-', "-", true)
-        Menu.addField('-', "-", true)
-      } else if (remainder === 2) {
-        Menu.addField('-', "-", true)
-      }
-    }
+    //   if (remainder === 0) {
+    //     return
+    //   } else if (remainder === 1) {
+    //     Menu.addField('-', "-", true)
+    //     Menu.addField('-', "-", true)
+    //   } else if (remainder === 2) {
+    //     Menu.addField('-', "-", true)
+    //   }
+    // }
 
-    NumberOfSounds()
+    // NumberOfSounds()
 
-    message.channel.send(Menu)
+    console.log(Menu);
+
+    message.channel.send({ embeds: [Menu] })
       .then((sentMessage) => {
         Object.values(Sounds).forEach(val => {
           sentMessage.react(val.emoji)
--- src/server.js
+++ src/server.js
@@ -1,26 +1,55 @@
 
 const fs = require('fs');
 const path = require('path');
-const { OpusEncoder } = require('@discordjs/opus');
-const Config = require('./config')
-const Commando = require('discord.js-commando')
-const client = new Commando.CommandoClient({
-  commandPrefix: Config.PREFIX,
-  owner: Config.OWNER
-})
+// const { OpusEncoder } = require('@discordjs/opus');
+const { clientId, guildId, token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 
-function PlaySoundOverVoiceChannel(VoiceChannel, Location, Name, Emoji) {
-  if (fs.existsSync(path.join(__dirname, `../assets/sounds/${Location}/${Name}_${Emoji}.mp3`))) {
-    VoiceChannel.join().then((connection) => {
-      connection.play(path.join(__dirname, `../assets/sounds/${Location}/${Name}_${Emoji}.mp3`))
-    })
-  } else {
-    msg.channel.send('Computron can not find the file you are looking for. Its probably your fault though.')
-  }
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, '../cmds');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-client.on('messageReactionAdd', (msg) => {
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+		console.log(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+  try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
+});
+
+client.on(Events.MessageReactionAdd, (msg) => {
   // Keeps audio from playing while bot adds initial reactions
   if (msg.count <= 1) {
     return
@@ -32,7 +61,7 @@ client.on('messageReactionAdd', (msg) => {
 
   let Sounds = {};
 
-  function RetreiveAudioClips(msg) {
+  function RetreiveAudioClips() {
     let i = 0;
     const folder = `./assets/sounds/${CurrentCharacter}`;
     fs.readdirSync(folder).forEach(file => {
@@ -57,21 +86,28 @@ client.on('messageReactionAdd', (msg) => {
   }
   // Plays sound
   if (results) {
-    const VoiceChannel = client.channels.cache.get(Config.CHANNEL)
+    const VoiceChannel = client.channels.cache.get(channel)
+
+    function PlaySoundOverVoiceChannel(VoiceChannel, Location, Name, Emoji) {
+      if (fs.existsSync(path.join(__dirname, `../assets/sounds/${Location}/${Name}_${Emoji}.mp3`))) {
+        VoiceChannel.join().then((connection) => {
+          connection.play(path.join(__dirname, `../assets/sounds/${Location}/${Name}_${Emoji}.mp3`));
+        })
+      } else {
+        console.log(`Could not play sound; file ${Location}/${Name}_${Emoji}.mp3 not found.`);
+        msg.channel.send('Could not play sound. Check bot logs for details.');
+      }
+    }
+
     PlaySoundOverVoiceChannel(VoiceChannel, results.character, results.name, results.emoji)
   }
-})
-
-client.login(Config.TOKEN)
-client.on('ready', () => {
-
-  client.registry
-    // .registerGroups([
-    //   ['soundboards', 'menus']
-    // ])
-    .registerDefaults()
-    .registerCommandsIn(path.join(__dirname, '../cmds'))
-
-  console.log(`${client.user.tag} has logged in.`);
 });
 
+client.once(Events.ClientReady, readyClient => {
+  // readyClient.registry
+  //   .registerCommandsIn(path.join(__dirname, '../cmds'))
+
+  console.log(`${readyClient.user.tag} has logged in.`);
+});
+
+client.login(token);
