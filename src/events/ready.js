bd80b3912c4360f9eb40bee658ddd174294d0110
Tim Wong <wongkk25@users.noreply.github.com>
2025-02-03 17:51:08 -0500

    connect bot to voice at startup if other users are already connected

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,24 @@
 const { Events } = require('discord.js');
+const { channelId } = require('../config.json');
+const { joinVoiceChannel } = require('@discordjs/voice');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
+		// if users already are in the voice channel, join it on start
+		const channel = await client.channels.fetch(channelId);
+		const numConnectedMembers = channel.members?.size || 0;
+
+		if (numConnectedMembers > 0) {
+			console.log(`Members already are connected; connecting bot to channel ${channel.id}, guild ${channel.guild.id}`);
+			joinVoiceChannel({
+				channelId: channel.id,
+				guildId: channel.guildId,
+				adapterCreator: channel.guild.voiceAdapterCreator,
+			});
+		}
+
 		console.log(`Bot logged in as ${client.user.tag}`);
 	},
 };
