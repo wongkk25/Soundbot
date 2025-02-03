44826ee3e59eb03868fd16fb71c4ac98b0c75b1b
Tim Wong <wongkk25@users.noreply.github.com>
2025-02-03 07:54:33 -0500

    reimplemented initial voice connection

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,22 @@
 const { Events } = require('discord.js');
+const { channelId } = require('../config.json');
+const { joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+		const channel = await client.channels.fetch(channelId);
+		const voiceConnection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guildId,
+			adapterCreator: channel.guild.voiceAdapterCreator,
+		});
+		
+		voiceConnection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
+			console.log('Connection is in the Ready state!');
+		});
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
